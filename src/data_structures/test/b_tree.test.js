782b666accdee7889f0c0aaabd7197b9ef0b8213
Dan Roberts <dan@adadevelopersacademy.org>
2020-06-09 10:02:25 -0700

    Finish tests for splitChild

--- src/data_structures/test/b_tree.test.js
+++ src/data_structures/test/b_tree.test.js
@@ -78,6 +78,80 @@ describe(BTree, () => {
       });
     });
 
-    
+    it('moves the child\'s middle k/v to the parent', () => {
+      const node = fillNode({ keyCount: minDegree, isLeaf: false });
+      const child = fillNode({ keyCount: minDegree * 2 - 1, isLeaf: true, keyPrefix: 'child' });
+
+      const childIndex = 2;
+      node.children[childIndex] = child;
+
+      const middleIndex = Math.floor(child.keys.length / 2)
+      const middleKey = child.keys[middleIndex];
+      const middleVal = child.values[middleIndex];
+
+      const beforeChildrenCount = node.children.length;
+      const beforeKeysCount = node.keys.length;
+      const beforeValsCount = node.values.length;
+      const beforeSibling = node.children[childIndex + 1];
+
+      btree._splitChild(node, childIndex);
+
+      expect(node.children.length).toBe(beforeChildrenCount + 1);
+      expect(node.keys.length).toBe(beforeKeysCount + 1);
+      expect(node.values.length).toBe(beforeValsCount + 1);
+
+      expect(node.keys[childIndex + 1]).toBe(middleKey);
+      expect(node.values[childIndex + 1]).toBe(middleVal);
+
+      expect(child.keys).not.toContain(middleKey);
+      expect(child.values).not.toContain(middleVal);
+
+      const sibling = node.children[childIndex + 1];
+      expect(sibling).not.toBe(beforeSibling);
+      expect(sibling.keys).not.toContain(middleKey);
+      expect(sibling.values).not.toContain(middleVal);
+    });
+
+    it('moves the second half of the child\'s k/vs to the sibling', () => {
+      const node = fillNode({ keyCount: minDegree, isLeaf: false });
+      const child = fillNode({ keyCount: minDegree * 2 - 1, isLeaf: true, keyPrefix: 'child' });
+
+      const childIndex = 2;
+      node.children[childIndex] = child;
+
+      const middleIndex = Math.floor(child.keys.length / 2)
+      const leftKeys = child.keys.slice(0, middleIndex);
+      const rightKeys = child.keys.slice(middleIndex + 1);
+      const leftVals = child.values.slice(0, middleIndex);
+      const rightVals = child.values.slice(middleIndex + 1);
+
+      btree._splitChild(node, childIndex);
+
+      expect(child.keys).toStrictEqual(leftKeys);
+      expect(child.values).toStrictEqual(leftVals);
+
+      const sibling = node.children[childIndex + 1];
+      expect(sibling.keys).toStrictEqual(rightKeys);
+      expect(sibling.values).toStrictEqual(rightVals);
+    });
+
+    it('moves the second half of the grandchildren to the sibling, if the child is an internal node', () => {
+      const node = fillNode({ keyCount: minDegree, isLeaf: false });
+      const child = fillNode({ keyCount: minDegree * 2 - 1, isLeaf: false, keyPrefix: 'child' });
+
+      const childIndex = 2;
+      node.children[childIndex] = child;
+
+      const middleIndex = child.children.length / 2;
+      const leftGrandchildren = child.children.slice(0, middleIndex);
+      const rightGrandchildren = child.children.slice(middleIndex);
+
+      btree._splitChild(node, childIndex);
+
+      expect(child.children).toStrictEqual(leftGrandchildren);
+
+      const sibling = node.children[childIndex + 1];
+      expect(sibling.children).toStrictEqual(rightGrandchildren);
+    });
   });
 });
